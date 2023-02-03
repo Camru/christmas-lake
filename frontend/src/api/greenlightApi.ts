a8913d7e072955e74664b0f814fa447cf1787607
Camden Rudisill <camden@Camdens-MacBook-Air.local>
2023-02-03 17:25:43 -0500

    udpates build

--- frontend/src/api/greenlightApi.ts
+++ frontend/src/api/greenlightApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {MediaType, MediaEntity, Tags} from '../types/types';
 
 const instance = axios.create({
-  baseURL: '/v1/',
+  baseURL: 'http://localhost:4000/v1/',
 });
 
 export type Rating = {
